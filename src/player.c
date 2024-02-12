1d1b344878b2fe04b9c3b56298d2ce7840c7b22a
DaStoopidGuy <dastoopidguy@protonmail.com>
2024-02-12 15:43:19 +0500

    player collsion function created

--- src/player.c
+++ src/player.c
@@ -13,10 +13,41 @@ Player NewPlayer(Vector2 pos, const char *texture_file_name)
     return player;
 }
 
+void CollidePlayerWithTile(Player *player, Rectangle tile_rec)
+{
+    if (CheckCollisionRecs(player->rec, tile_rec))
+    { // Collision with tile detected, stop vertical movement
+        if (player->vel.y > 0 && player->pos.y + player->rec.height - player->vel.y <= tile_rec.y)
+        {
+            // Player is falling and collides with the top of the platform
+            player->pos.y = tile_rec.y - player->rec.height;
+            player->vel.y = 0;
+            player->is_on_ground = true;
+        }
+        else if (player->vel.y < 0 && player->pos.y - player->vel.y >= tile_rec.y + tile_rec.height)
+        {
+            // Player is jumping and collides with the bottom of the platform
+            player->pos.y = tile_rec.y + tile_rec.height;
+            player->vel.y = 0;
+        }
+        else if (player->vel.x > 0 && player->pos.x + player->rec.width - player->vel.x <= tile_rec.x)
+        {
+            // Player is moving right and collides with the left side of the platform
+            player->pos.x = tile_rec.x - player->rec.width;
+        }
+        else if (player->vel.x < 0 && player->pos.x - player->vel.x >= tile_rec.x + tile_rec.width)
+        {
+            // Player is moving left and collides with the right side of the platform
+            player->pos.x = tile_rec.x + tile_rec.width;
+        }
+    }
+}
 void UpdatePlayer(Player *player, float deltaTime, int tilemap[][25], int rows, int cols, bool godmode)
 {
     // Horizontal movement
-    int player_x_direction = ((IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) - (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)));
+    bool right_key = IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D);
+    bool left_key = IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A);
+    int player_x_direction = (right_key - left_key);
     player->vel.x = PLAYER_H_SPD * player_x_direction * deltaTime;
 
     if (godmode)
@@ -33,14 +64,6 @@ void UpdatePlayer(Player *player, float deltaTime, int tilemap[][25], int rows, 
             player->is_on_ground = false;
         }
 
-        // remove later
-        // if (player->pos.y > 400)
-        // {
-        //     player->vel.y = 0;
-        //     player->pos.y = 400;
-        //     player->is_on_ground = true;
-        // }
-        // else if (player->pos.y < 400)
         if (!player->is_on_ground)
             player->vel.y += GRAVITY * deltaTime;
 
@@ -49,35 +72,7 @@ void UpdatePlayer(Player *player, float deltaTime, int tilemap[][25], int rows, 
             for (int x = 0; x < cols; x++)
             {
                 if (tilemap[y][x] != 0) // Check for non-empty tiles
-                {
-                    Rectangle tile_rec = {x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE};
-                    if (CheckCollisionRecs(player->rec, tile_rec))
-                    { // Collision with tile detected, stop vertical movement
-                        if (player->vel.y > 0 && player->pos.y + player->rec.height - player->vel.y <= tile_rec.y)
-                        {
-                            // Player is falling and collides with the top of the platform
-                            player->pos.y = tile_rec.y - player->rec.height;
-                            player->vel.y = 0;
-                            player->is_on_ground = true;
-                        }
-                        else if (player->vel.y < 0 && player->pos.y - player->vel.y >= tile_rec.y + tile_rec.height)
-                        {
-                            // Player is jumping and collides with the bottom of the platform
-                            player->pos.y = tile_rec.y + tile_rec.height;
-                            player->vel.y = 0;
-                        }
-                        else if (player->vel.x > 0 && player->pos.x + player->rec.width - player->vel.x <= tile_rec.x)
-                        {
-                            // Player is moving right and collides with the left side of the platform
-                            player->pos.x = tile_rec.x - player->rec.width;
-                        }
-                        else if (player->vel.x < 0 && player->pos.x - player->vel.x >= tile_rec.x + tile_rec.width)
-                        {
-                            // Player is moving left and collides with the right side of the platform
-                            player->pos.x = tile_rec.x + tile_rec.width;
-                        }
-                    }
-                }
+                    CollidePlayerWithTile(player, (Rectangle){x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE});
             }
         }
     }
